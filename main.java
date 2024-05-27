7c46c9e1df37740bb804d7210a8ccae86b8e945d
augustoristow1 <augustoisantos@hotmail.com>
2024-05-27 16:09:51 -0300

    Update main.java

--- main.java
+++ main.java
@@ -6,7 +6,7 @@ public class Main {
         String nome = "Caue Caina";
         double saldo = 2500.00;
         String tipoDeConta = "Corrente";
-        int operacao;
+        int operacao = 0;
         double deposito = 0;
         double pix = 0;
         String pixChave;
@@ -26,49 +26,51 @@ public class Main {
         System.out.println(" ");
         System.out.println(" ");
 
-
-        System.out.println("Operações: ");
-        System.out.println(" ");
-        System.out.println("1- Consultar saldo ");
-        System.out.println("2- Fazer deposito ");
-        System.out.println("3- transferencia Pix");
-        System.out.println("4- Sair ");
-        System.out.println(" ");
-        System.out.println("Digite a operação desejada: ");
-        operacao = op.nextInt();
-
-        if (operacao == 1){
-            System.out.println("Você tem R$ " + saldo);
-
-        } else if (operacao == 2) {
-            System.out.println("Qual o valor do depósito? ");
-            deposito = valor1.nextDouble();
-            System.out.println("Gerando boleto de deposito no valor de: R$ "+ deposito);
-            System.out.println(" ");
-            System.out.println("BOLETO IMAGINARIO");
-            System.out.println(" ");
-            saldo = saldo + deposito;
-            System.out.println("Valor de saldo atulizado para: " + saldo);
+        String menu = """
+                1 - Consultar saldo
+                2 - Depósito
+                3 - Transferencia Pix
+                4 - Sair
+                """;
 
 
-        } else if (operacao == 3) {
-            System.out.println("Qual o valor que deseja transferir:");
-            pix = valor2.nextDouble();
-        } else if (pix > saldo) {
-            System.out.println("Você não tem saldo disponivel para essa operação");
-        } else {
-            System.out.println("Valor da transferência: R$ "+ pix);
-            System.out.println("Insira a chave pix destinatária: ");
-            pixChave = chave.nextLine();
-            System.out.println("Confirme a chave: " + pixChave);
+            while (operacao != 4) {
+                System.out.println("Selecione a operação que deseja:");
+                System.out.println("\n" + menu);
+                operacao = op.nextInt();
 
-        } if ( operacao == 4){
-            System.out.println("Deseja mesmo sair do sistema? (s ou n) ");
-            sair = op.nextBoolean();
+                if (operacao == 1) {
+                System.out.println("Você tem R$ " + saldo);
+
+                } else if (operacao == 2) {
+
+                    System.out.println("Qual o valor do depósito? ");
+                    deposito = valor1.nextDouble();
+                    System.out.println("Gerando boleto de deposito no valor de: R$ " + deposito);
+                    System.out.println(" ");
+                    System.out.println("BOLETO IMAGINARIO");
+                    System.out.println(" ");
+                    saldo = saldo + deposito;
+                    System.out.println("Valor de saldo atulizado para: " + saldo);
+
+                }else if (operacao == 3) {
+                        System.out.println("Qual o valor que deseja transferir:");
+                        pix = valor2.nextDouble();
+                        if (pix > saldo) {
+                            System.out.println("Você não tem saldo disponivel para essa operação");
+                        }else {
+                            System.out.println("\nValor da transferência: R$ " + pix);
+                            System.out.println("\nInsira a chave pix destinatária: ");
+                            pixChave = chave.nextLine();
+                            System.out.println("\nConfirme a chave: " + pixChave);
+                            saldo = saldo - pix;
+                            System.out.println("\n\nSeu saldo restante é: " + saldo);
+                        }
+                } else if (operacao != 4) {
+                    System.out.println("opção invalida");
+                }
 
 
-        }
-
-
+            }
     }
 }
