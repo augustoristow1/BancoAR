6c55351dbf918f0873e346322ae8d2a48c9e58f1
augustoristow1 <augustoisantos@hotmail.com>
2024-05-27 10:29:50 -0300

    Update main.java

--- main.java
+++ main.java
@@ -7,9 +7,10 @@ public class Main {
         double saldo = 2500.00;
         String tipoDeConta = "Corrente";
         int operacao;
-        int cobranca = 0;
-        int pix = 0;
+        double deposito = 0;
+        double pix = 0;
         String pixChave;
+        boolean sair = false;
 
         Scanner valor1 = new Scanner(System.in);
         Scanner op = new Scanner(System.in);
@@ -29,7 +30,7 @@ public class Main {
         System.out.println("Operações: ");
         System.out.println(" ");
         System.out.println("1- Consultar saldo ");
-        System.out.println("2- Enviar cobrança ");
+        System.out.println("2- Fazer deposito ");
         System.out.println("3- transferencia Pix");
         System.out.println("4- Sair ");
         System.out.println(" ");
@@ -38,18 +39,32 @@ public class Main {
 
         if (operacao == 1){
             System.out.println("Você tem R$ " + saldo);
-        } else if (operacao == 2) {
-            System.out.println("Qual o valor da cobrança? ");
-            cobranca = valor1.nextInt();
-            System.out.println("Gerando Link de cobrança no valor de: R$ "+ cobranca);
-            System.out.println("LINK IMAGINARIO TEMPORARIO");
 
-        } else if (operacao == 3){
+        } else if (operacao == 2) {
+            System.out.println("Qual o valor do depósito? ");
+            deposito = valor1.nextDouble();
+            System.out.println("Gerando boleto de deposito no valor de: R$ "+ deposito);
+            System.out.println(" ");
+            System.out.println("BOLETO IMAGINARIO");
+            System.out.println(" ");
+            saldo = saldo + deposito;
+            System.out.println("Valor de saldo atulizado para: " + saldo);
+
+
+        } else if (operacao == 3) {
             System.out.println("Qual o valor que deseja transferir:");
-            pix = valor2.nextInt();
+            pix = valor2.nextDouble();
+        } else if (pix > saldo) {
+            System.out.println("Você não tem saldo disponivel para essa operação");
+        } else {
             System.out.println("Valor da transferência: R$ "+ pix);
             System.out.println("Insira a chave pix destinatária: ");
             pixChave = chave.nextLine();
+            System.out.println("Confirme a chave: " + pixChave);
+
+        } if ( operacao == 4){
+            System.out.println("Deseja mesmo sair do sistema? (s ou n) ");
+            sair = op.nextBoolean();
 
 
         }
